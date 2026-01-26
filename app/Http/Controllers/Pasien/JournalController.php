24a98fdcd8289b0c5ac24da7128007bb27ca66c8
Wildhan <95916884+wildhanry@users.noreply.github.com>
2026-01-26 15:26:42 +0700

    Change AI service URL to environment variable
    Updated AI service URL to use environment variable.

--- app/Http/Controllers/Pasien/JournalController.php
+++ app/Http/Controllers/Pasien/JournalController.php
@@ -41,7 +41,7 @@ class JournalController extends Controller
 
         try {
             // Call the external Python AI Service
-            $aiServiceUrl = env('https://www.pythonanywhere.com/user/wildhanry/webapps/#tab_id_wildhanry_pythonanywhere_com');
+            $aiServiceUrl = env('AI_SERVICE_URL');
             
             if (!$aiServiceUrl) {
                 throw new \Exception('AI_SERVICE_URL not configured in .env file');
